077aa95552f6069f1355f495fd18a37ee89a1966
wudidatao <82302925@qq.com>
2019-03-17 23:17:52 +0800

    Update var.sh

--- var.sh
+++ var.sh
@@ -6,6 +6,7 @@ days=7
 base_backup_time=`date "+%G-%m-%d"`
 base_backup_dir=`find $backup_dir -name $base_backup_time -ctime -$days`
 host_ip=192.168.1.12
+remote_ip=192.168.1.13
 port=3306
 user=root
 pass=123456
