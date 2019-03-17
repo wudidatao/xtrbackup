2ca0b3c3add0bb69b8713694286768f036fa1a87
wudidatao <82302925@qq.com>
2019-03-18 00:02:51 +0800

    Update run_binlog.sh

--- test/run_binlog.sh
+++ test/run_binlog.sh
@@ -9,3 +9,5 @@ echo $binlog_position
 
 #GTID模式下，需要使用--skip-gtids忽略GTID，否则可能会出现无法执行binlog的问题
 mysqlbinlog --skip-gtids --start-position=$binlog_position /data/mysql/5.7.18-3306/data/$binlog_filename | mysql -h192.168.1.12 -uroot -p123456
+
+mysqlbinlog --base64-output=decode-rows -v --start-position="56174" --stop-position="57752" mysql-bin.000012 | mysql -h192.168.1.12 -uroot -p -v
