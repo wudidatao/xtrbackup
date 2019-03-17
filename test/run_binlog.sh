cea043c0d32fb6d8f29d053379c716290731ae2a
wudidatao <82302925@qq.com>
2019-03-17 23:43:02 +0800

    Update run_binlog.sh

--- test/run_binlog.sh
+++ test/run_binlog.sh
@@ -7,4 +7,5 @@ echo $binlog_filename
 binlog_position=`cat /data/mysql/5.7.18-3306/data/xtrabackup_binlog_pos_innodb | awk '{print $2}'`
 echo $binlog_position
 
+#GTID模式下，需要使用--skip-gtids忽略GTID，否则可能会出现无法执行binlog的问题
 mysqlbinlog --skip-gtids --start-position=$binlog_position /data/mysql/5.7.18-3306/data/$binlog_filename | mysql -h192.168.1.12 -uroot -p123456
