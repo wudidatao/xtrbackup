59e134c3cfd878b9c790300278b71950a3c65425
wudidatao <82302925@qq.com>
2019-03-17 23:12:57 +0800

    Create var.sh

---
+++ var.sh
@@ -0,0 +1,10 @@
+backup_dir=/data/backups
+days=7
+base_backup_time=`date "+%G-%m-%d"`
+base_backup_dir=`find $backup_dir -name $base_backup_time -ctime -$days`
+host_ip=192.168.1.12
+port=3306
+user=root
+pass=123456
+data_dir=/data/mysql/5.7.18-3306/data/
+database_name="mysql performance_schema sys test"
